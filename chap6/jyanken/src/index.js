dbf83a150af9892371b8f9bee41a78ec4fb8c48a
evofan <evofan@outlook.jp>
2020-11-27 00:09:11 +0900

    fix

--- chap6/jyanken/src/index.js
+++ chap6/jyanken/src/index.js
@@ -4,21 +4,29 @@ import PropTypes from 'prop-types';// ■コンポーネントに渡す引数を
 import './index.css';
 
 class JyankenGamePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { human: null, computer: null }
+  constructor(props) { // ■ES6のコンストラクタ、※ReactコンポーネントではPropsを受け取る
+    super(props); // ■最初にスーパークラスのコンストラクタを呼び出す、オブジェクト指向でよくあるパターン
+    this.state = { human: null, computer: null } // ■this.stateに初期値を代入、後で他クラスで動的に設定も出来るが、
+    // ★可読性を考慮して、コンストラクタで初期値（適切な値が無ければNull）を設定しておく事を推奨
   }
 
+  /**
+   * 人間の手を受け取り、ジャンケンを行う
+   * @param { number } human_hand 
+   */
   pon(human_hand) {
-    const computer_hand = Math.floor(Math.random() * 3);
-    this.setState({ human: human_hand, computer: computer_hand });
+    const computer_hand = Math.floor(Math.random() * 3); // ■乱数を使ってCPU側のてを設定する
+    this.setState({ human: human_hand, computer: computer_hand }); // ■人間の手、CPUの手をステートに格納
   }
 
+  /**
+   * ジャンケンの勝敗を判定する
+   */
   judge() {
-    if (this.state.human === null) {
+    if (this.state.human === null) { // 人間の手が設定されてない場合はnullを返す
       return null;
     } else {
-      return (this.state.computer - this.state.human + 3) % 3;
+      return (this.state.computer - this.state.human + 3) % 3;// ■引き分けなら0、人間の勝ちなら1、CPU勝ちなら2を返す＝アルゴリズム
     }
   }
 
@@ -46,9 +54,14 @@ JyankenBox.propTypes = {
   actionPon: PropTypes.func
 }
 
+/**
+ * 人間の手、CPUの手、勝敗を表示する
+ * @param { object } props 
+ */
 const ScoreBox = (props) => {
   const teString = ["グー", "チョキ", "パー"];
   const judgementString = ["引き分け", "勝ち", "負け"];
+  // 0-2の手を文字列表示に変換、0-2の勝敗結果を文字列表示に変換、
   return (
     <div>
       <table>
